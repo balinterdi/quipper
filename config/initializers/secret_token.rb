dca9ce727723b8e3c12d1ff6090d22a7633978a3
Balint Erdi <balint.erdi@gmail.com>
2013-07-18 08:58:22 +0200

    Ember in a Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quipper::Application.config.secret_token = '30256aea5636efaa67e14ac5c1333a890c5076c2fe17b1099edf706c5b07e6fa60d5cd952d749e6edd0edf11b3447d740fdabd4afa9da22fed1b6847005d0716'
