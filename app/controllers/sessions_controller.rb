3f151398f8aeb5df1e2c5c17e0cc92036a529bee
Balint Erdi <balint.erdi@gmail.com>
2013-07-21 15:24:45 +0200

    Create a user object from twitter oauth callback hash
     * Also upgrade to Rails 4 in the process

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,36 @@
 class SessionsController < ApplicationController
   def create
-    puts "Auth hash"
-    p auth_hash
+    update_user
     redirect_to root_path
   end
 
 private
+  def update_user
+    user = User.first(conditions: { nickname: auth_hash.info.nickname })
+    unless user
+      user = User.new
+    end
+    user.update_attributes(user_attributes)
+  end
+
+  def user_attributes
+    info = auth_hash.info
+    creds = auth_hash.credentials
+    {
+      oauth_token: creds.token,
+      oauth_token_secret: creds.secret,
+      nickname: info.nickname,
+      name: info.name,
+      avatar_url: info.image,
+      location: info.location,
+      website: info.urls.Website,
+      description: info.description
+    }
+  end
+
   def auth_hash
     request.env['omniauth.auth']
   end
+
+
 end
