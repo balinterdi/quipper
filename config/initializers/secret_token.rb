bbe6f9f3eae33bfb90b6254a82df433902ff3ae4
Balint Erdi <balint.erdi@gmail.com>
2013-07-29 10:00:37 +0200

    Upgrade to Rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Quipper::Application.config.secret_token = '30256aea5636efaa67e14ac5c1333a890c5076c2fe17b1099edf706c5b07e6fa60d5cd952d749e6edd0edf11b3447d740fdabd4afa9da22fed1b6847005d0716'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quipper::Application.config.secret_key_base = '77c2cd0eff67653ee9af01012161722a47d8125d1a211f790cb83eec7bd590e461aab7b596c8ce9e9383a43354665b472392fde7424ee468dc396ca903d32768'
