4a4abd6b71d93676e963f10354f305c7469deb03
Balint Erdi <balint.erdi@gmail.com>
2013-08-05 09:48:20 +0200

    Parse urls in entities.urls

--- app/assets/javascripts/ember-app.js
+++ app/assets/javascripts/ember-app.js
@@ -12,8 +12,19 @@ App.TweetController = Ember.ObjectController.extend({
   }.property('user.profile_image_url')
 });
 
-Ember.Handlebars.helper('html', function(text, options) {
-  return new Handlebars.SafeString(text.replace(/\n/g, '<br />'));
+Ember.Handlebars.helper('html', function(tweet, options) {
+  var text = tweet.get('text').replace(/\n/g, '<br />');
+  var urlEntities = tweet.get('entities.urls');
+  var withUrls = text;
+  urlEntities.forEach(function(urlData) {
+    //FIXME: This breaks when there are multiple urls
+    //TODO:  For retweets entities.urls is empty. The indices are in entities.user_mentions.indices
+    // and refer to the original tweet
+    var start = urlData.indices[0];
+    var end   = urlData.indices[1];
+    withUrls = text.slice(0, start) + '<a href="' + urlData.expanded_url + '">' + urlData.display_url + '</a>' + text.slice(end);
+  });
+  return new Handlebars.SafeString(withUrls);
 });
 
 
