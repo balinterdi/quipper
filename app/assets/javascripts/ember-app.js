41a1fef43b707b01a570720cc9be377ce520c313
Balint Erdi <balint.erdi@gmail.com>
2013-08-06 09:17:46 +0200

    Don't hardcode my twitter id in the url to fetch home timeline

--- app/assets/javascripts/ember-app.js
+++ app/assets/javascripts/ember-app.js
@@ -2,7 +2,7 @@ window.App = Ember.Application.create();
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return Ember.$.getJSON('/timelines/baaz.json');
+    return Ember.$.getJSON('/timelines/home.json');
   }
 });
 
